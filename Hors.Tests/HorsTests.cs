be24f5b05704c2c9bc05e2e60ef7cf90674c29b1
Denis Peshekhonov <denis@peshekhonov.ru>
2019-08-26 02:15:32 +0300

    holiday test

--- Hors.Tests/HorsTests.cs
+++ Hors.Tests/HorsTests.cs
@@ -1,3 +1,6 @@
+using System;
+using System.Linq;
+using Hors.Models;
 using NUnit.Framework;
 
 namespace Hors.Tests
@@ -10,9 +13,31 @@ namespace Hors.Tests
         }
 
         [Test]
-        public void Test1()
+        public void TestHolidays()
         {
-            Assert.Pass();
+            var parser = new HorsTextParser();
+            var result = parser.Parse("в эти выходные еду на дачу", new DateTime(2019, 9, 2));
+            
+            Assert.AreEqual(1, result.Dates.Count);
+            var date = result.Dates.First();
+
+            Assert.AreEqual(DateTimeTokenType.Period, date.Type);
+            Assert.AreEqual(7, date.DateFrom.Day);
+            Assert.AreEqual(8, date.DateTo.Day);
+        }
+        
+        [Test]
+        public void TestHoliday()
+        {
+            var parser = new HorsTextParser();
+            var result = parser.Parse("пойду гулять в следующий выходной", new DateTime(2019, 9, 2));
+            
+            Assert.AreEqual(1, result.Dates.Count);
+            var date = result.Dates.First();
+
+            Assert.AreEqual(DateTimeTokenType.Fixed, date.Type);
+            Assert.AreEqual(14, date.DateFrom.Day);
+            Assert.AreEqual(14, date.DateTo.Day);
         }
     }
 }
